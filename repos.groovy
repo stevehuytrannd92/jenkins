78b184e3d70f977a346bea5cb6a3f166f7c718ef
Steve <stevehuytrannd92@gmail.com>
2025-08-15 17:03:05 +0700

    fix repo name

--- repos.groovy
+++ repos.groovy
@@ -1,15 +1,21 @@
 // repos.groovy
 return [
     [
-        url: 'git@github.com-stevehuytrannd92:stevehuytrannd92/presale.git',
+        url: 'git@github.com:stevehuytrannd92/presale.git',
         branch: 'maxidoge',
         folder: 'maxidoge',
         credId: 'stevehuytrannd92'
     ],
     [
-        url: 'git@github.com-stevehuytrannd92:stevehuytrannd92/presale.git',
+        url: 'git@github.com:stevehuytrannd92/presale.git',
         branch: 'btcswift',
         folder: 'btcswift',
+        credId: 'stevehuytrannd92'
+    ],
+    [
+        url: 'git@github.com:stevehuytrannd92/flockerz.git',
+        branch: 'btchyper',
+        folder: 'btchyper',
         credId: 'stevehuytrannd92'
     ],
     
