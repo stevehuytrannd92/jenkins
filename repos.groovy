16bda57ee82dc3d8a7e58b38126075a5a231d3f6
Steve <stevehuytrannd92@gmail.com>
2025-08-15 17:27:10 +0700

    update urls

--- repos.groovy
+++ repos.groovy
@@ -1,13 +1,13 @@
 // repos.groovy
 return [
     [
-        url: 'git@github.com:stevehuytrannd92/presale.git',
+        url: 'git@github.com-stevehuytrannd92:stevehuytrannd92/presale.git',
         branch: 'maxidoge',
         folder: 'maxidoge',
         credId: 'id_ed25519_stevehuytrannd92'
     ],
     [
-        url: 'git@github.com:stevehuytrannd92/presale.git',
+        url: 'git@github.com-stevehuytrannd92:stevehuytrannd92/presale.git',
         branch: 'btcswift',
         folder: 'btcswift',
         credId: 'id_ed25519_stevehuytrannd92'
