2aa7d10c34fad7cd100062553feb79a50ba4dff6
Steve <stevehuytrannd92@gmail.com>
2025-08-17 14:36:14 +0700

    add new site

--- repos.groovy
+++ repos.groovy
@@ -42,9 +42,9 @@ return [
         envs: [
             [ 
                 name: 'btcswifts', 
+                MAIN_DOMAIN:'https://btcswifts.com/',
                 BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
                 LOGO_PATH:'/img/btcswift/bitcoin-swift-logo-main.webp',
-                MAIN_DOMAIN:'https://btcswifts.com/',
                 MAIN_GTAG:'G-TB2PNE8KD4',
                 MAIN_SITENAME:'btcswift',
                 MAIN_TWITTER:'@btcswift',
@@ -65,13 +65,37 @@ return [
         envs: [
             [ 
                 name: 'wepetoken', 
+                MAIN_DOMAIN:'https://wepetoken.com/',
                 BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
                 LOGO_PATH:'/img/wepe/token.svg',
-                MAIN_DOMAIN:'https://wepetoken.com/',
                 MAIN_GTAG:'G-LJLVSX3D6T',
                 MAIN_SITENAME:'wepetoken',
                 MAIN_TWITTER:'@wepetoken',
                 OG_PATH:'/img/wepe/OG.jpeg'
+            ],
+        ]
+
+    ],
+    [
+        url: 'git@github.com:stevehuytrannd92/presale.git',
+        branch: 'token6900',
+        folder: 'token6900',
+        credId: 'id_ed25519_stevehuytrannd92',
+        vpsCredId: 'vps1',
+        vpsUser: 'ubuntu',
+        vpsHost: '165.154.235.205',
+        webrootBase: '/var/www/presale',
+
+        envs: [
+            [ 
+                name: 'spx6900token', 
+                MAIN_DOMAIN:'https://spxtoken6900.com/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/token6900/favicon.png',
+                MAIN_GTAG:'G-15Q5N6TLHT',
+                MAIN_SITENAME:'token6900',
+                MAIN_TWITTER:'@token6900',
+                OG_PATH:'/img/token6900/OG.jpeg'
             ],
         ]
 
