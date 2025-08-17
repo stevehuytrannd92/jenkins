a02e2c3656f09884848af7be0dd20cf793b04d90
Steve <stevehuytrannd92@gmail.com>
2025-08-17 16:29:28 +0700

    remove unsucess domain

--- repos.groovy
+++ repos.groovy
@@ -88,16 +88,6 @@ return [
 
         envs: [
             [ 
-                name: 'spxtoken6900', 
-                MAIN_DOMAIN:'https://spxtoken6900.com/',
-                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-                LOGO_PATH:'/img/token6900/favicon.png',
-                MAIN_GTAG:'G-15Q5N6TLHT',
-                MAIN_SITENAME:'token6900',
-                MAIN_TWITTER:'@token6900',
-                OG_PATH:'/img/token6900/OG.jpeg'
-            ],
-            [ 
                 name: 'token6969', 
                 MAIN_DOMAIN:'https://token6969.com/',
                 BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
