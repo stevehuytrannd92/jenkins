aacd1a91f966f26f0ca32b117403fb5669b513b8
Steve <stevehuytrannd92@gmail.com>
2025-08-18 13:33:37 +0700

    remove vps config

--- repos.groovy
+++ repos.groovy
@@ -1,34 +1,5 @@
 // repos.groovy
 return [
-    // [
-    //     url: 'git@github.com:stevehuytrannd92/presale.git',
-    //     branch: 'maxidoge',
-    //     folder: 'maxidoge',
-    //     credId: 'id_ed25519_stevehuytrannd92',
-    //     envs: [
-    //         [ 
-    //             name: 'maxidoge', 
-    //             BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-    //             LOGO_PATH:'/img/maxidoge/logo.svg',
-    //             MAIN_DOMAIN:'https://maxidoge.io',
-    //             MAIN_GTAG:'G-BS0FWQNNNZ',
-    //             MAIN_SITENAME:'maxidoge',
-    //             MAIN_TWITTER:'@maxidoge',
-    //             OG_PATH:'/img/maxidoge/OG.jpeg'
-    //         ],
-    //         [ 
-    //             name: 'maxidogecoin', 
-    //             BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-    //             LOGO_PATH:'/img/maxidoge/logo.svg',
-    //             MAIN_DOMAIN:'https://maxidogecoin.com',
-    //             MAIN_GTAG:'G-BS0FWQNNNZ',
-    //             MAIN_SITENAME:'maxidoge',
-    //             MAIN_TWITTER:'@maxidoge',
-    //             OG_PATH:'/img/maxidoge/OG.jpeg'
-    //         ]
-    //     ]
-
-    // ],
     [
         url: 'git@github.com:stevehuytrannd92/presale.git',
         branch: 'btcswift',
@@ -51,6 +22,103 @@ return [
         ]
 
     ],
+    [
+        url: 'git@github.com:trungplq/flockerz.git',
+        branch: 'wepe',
+        folder: 'wepe',
+        credId: 'id_ed25519_stevehuytrannd92',
+
+        vpsRef : 'vps1',   // 👈 just reference which VPS to use
+        envs: [
+            [ 
+                name: 'wepetoken', 
+                MAIN_DOMAIN:'https://wepetoken.com/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/wepe/token.svg',
+                MAIN_GTAG:'G-LJLVSX3D6T',
+                MAIN_SITENAME:'wepetoken',
+                MAIN_TWITTER:'@wepetoken',
+                OG_PATH:'/img/wepe/OG.jpeg'
+            ],
+        ]
+
+    ],
+    [
+        url: 'git@github.com:stevehuytrannd92/presale.git',
+        branch: 'token6900',
+        folder: 'token6900',
+        credId: 'id_ed25519_stevehuytrannd92',
+
+        vpsRef : 'vps1',   // 👈 just reference which VPS to use
+
+        envs: [
+            [ 
+                name: 'token6969', 
+                MAIN_DOMAIN:'https://token6969.com/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/token6900/favicon.png',
+                MAIN_GTAG:'G-15Q5N6TLHT',
+                MAIN_SITENAME:'token6900',
+                MAIN_TWITTER:'@token6900',
+                OG_PATH:'/img/token6900/OG.jpeg'
+            ],
+        ]
+
+    ],
+    [
+        url: 'git@github.com:stevehuytrannd92/flockerz.git',
+        branch: 'btchyper',
+        folder: 'btchyper',
+        credId: 'id_ed25519_stevehuytrannd92',
+
+        vpsRef : 'vps1',   // 👈 just reference which VPS to use
+        envs: [
+            [ 
+                name: 'btchyper', 
+                MAIN_DOMAIN:'https://btchyper.io/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/btchyper/token.svg',
+                MAIN_GTAG:'G-RKFMME3BTM',
+                MAIN_SITENAME:'btchyper',
+                MAIN_TWITTER:'@btchyper',
+                OG_PATH:'/img/btchyper/OG.jpeg'
+            ],
+        ]
+
+    ],
+    
+    [
+        url: 'git@github.com:stevehuytrannd92/flockerz.git',
+        branch: 'btcbull-btcbullcoin',
+        folder: 'btcbullcoin',
+        credId: 'id_ed25519_stevehuytrannd92',
+
+        vpsRef : 'vps1',   // 👈 just reference which VPS to use
+        envs: [
+            [ 
+                name: 'thebtcbull', 
+                MAIN_DOMAIN:'thebtcbull.com',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks3.json',
+                LOGO_PATH:'/img/btcbull/logo.png',
+                MAIN_GTAG:'G-GR6Q9PM18Q',
+                MAIN_SITENAME:'btcbull',
+                MAIN_TWITTER:'@btcbull',
+                OG_PATH:'img/btcbull/OG.jpeg'
+            ],
+            [ 
+                name: 'btcbullcoin_io', 
+                MAIN_DOMAIN:'btcbullcoin.io',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks3.json',
+                LOGO_PATH:'/img/btcbull/logo.png',
+                MAIN_GTAG:'G-GR6Q9PM18Q',
+                MAIN_SITENAME:'btcbull',
+                MAIN_TWITTER:'@btcbull',
+                OG_PATH:'img/btcbull/OG.jpeg'
+            ],
+        ]
+
+    ],
+
     
 
 ]
