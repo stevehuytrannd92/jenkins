9f2b752703f10541d7298e262f9bb69833c9a2c5
Steve <stevehuytrannd92@gmail.com>
2025-08-18 15:56:26 +0700

    add new site

--- repos.groovy
+++ repos.groovy
@@ -2,6 +2,46 @@
 return [
     [
         url: 'git@github.com:stevehuytrannd92/presale.git',
+        branch: 'maxidoge',
+        folder: 'maxidoge',
+        credId: 'id_ed25519_stevehuytrannd92',
+
+        vpsRef : 'vps1',   // 👈 just reference which VPS to use
+
+        envs: [
+            [ 
+                MAIN_DOMAIN:'https://maxidoge.io/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/maxidoge/logo.svg',
+                MAIN_GTAG:'G-BS0FWQNNNZ',
+                MAIN_SITENAME:'maxidoge',
+                MAIN_TWITTER:'@maxidoge',
+                OG_PATH:'/img/maxidoge/OG.jpeg'
+            ],
+            [ 
+                MAIN_DOMAIN:'https://maxidogecoin.com/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/maxidoge/logo.svg',
+                MAIN_GTAG:'G-BS0FWQNNNZ',
+                MAIN_SITENAME:'maxidoge',
+                MAIN_TWITTER:'@maxidoge',
+                OG_PATH:'/img/maxidoge/OG.jpeg'
+            ],
+            [ 
+                MAIN_DOMAIN:'https://maxidogtoken.com/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/maxidoge/logo.svg',
+                MAIN_GTAG:'G-BS0FWQNNNZ',
+                MAIN_SITENAME:'maxidoge',
+                MAIN_TWITTER:'@maxidoge',
+                OG_PATH:'/img/maxidoge/OG.jpeg'
+            ],
+        ]
+
+    ],
+
+    [
+        url: 'git@github.com:stevehuytrannd92/presale.git',
         branch: 'btcswift',
         folder: 'btcswift',
         credId: 'id_ed25519_stevehuytrannd92',
