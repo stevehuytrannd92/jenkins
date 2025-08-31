6ec777aa4c48a1cec2463626ac36d53956c718e9
Steve <stevehuytrannd92@gmail.com>
2025-08-31 13:37:39 +0700

    remove redundants

--- repos.groovy
+++ repos.groovy
@@ -4,9 +4,6 @@ def loadRepos(String name) {
 
 def repos = []
 repos += load "${env.WORKSPACE}/gitRepos/maxidoge.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/btcswift.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/wepe.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/token6900.groovy"
 repos += load "${env.WORKSPACE}/gitRepos/btchyper.groovy"
 repos += load "${env.WORKSPACE}/gitRepos/btcbullcoin.groovy"
 repos += load "${env.WORKSPACE}/gitRepos/snorter.groovy"
