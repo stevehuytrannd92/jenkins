c6e787e516836f2b691d3c74f1b11fd13fd4461e
Steve <stevehuytrannd92@gmail.com>
2025-09-14 22:40:46 +0700

    add new repo

---
+++ gitRepos/dogeverse.groovy
@@ -0,0 +1,24 @@
+return [
+    [
+        url: 'git@github.com:stevehuytrannd92/presale.git',
+        branch: 'dogeverse',
+        folder: 'dogeverse',
+        credId: 'id_ed25519_stevehuytrannd92',
+        buildType: 'nextjs',
+
+        vpsRef : 'vps1',   // 👈 just reference which VPS to use
+        envs: [
+            [ 
+                MAIN_DOMAIN:'https://thedogverse.io/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/dogeverse/token.svg',
+                MAIN_GTAG:'G-KYB4XZ3NGN',
+                MAIN_SITENAME:'thedogverse',
+                MAIN_TWITTER:'@thedogverse',
+                OG_PATH:'/img/dogeverse/OG.jpeg'
+            ],
+          
+        ]
+
+    ],
+]
