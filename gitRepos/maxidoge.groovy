2d6a39324b9f32acff0f8bbe98632e540784ec12
Steve <stevehuytrannd92@gmail.com>
2025-09-14 22:41:21 +0700

    remove dead repo

--- gitRepos/maxidoge.groovy
+++ gitRepos/maxidoge.groovy
@@ -10,33 +10,7 @@ return [
         vpsRef : 'vps1',   // 👈 just reference which VPS to use
 
         envs: [
-            [ 
-                MAIN_DOMAIN:'https://maxidoge.io/',
-                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-                LOGO_PATH:'/img/maxidoge/logo.svg',
-                MAIN_GTAG:'G-BS0FWQNNNZ',
-                MAIN_SITENAME:'maxidoge',
-                MAIN_TWITTER:'@maxidoge',
-                OG_PATH:'/img/maxidoge/OG.jpeg'
-            ],
-            [ 
-                MAIN_DOMAIN:'https://maxidogecoin.com/',
-                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-                LOGO_PATH:'/img/maxidoge/logo.svg',
-                MAIN_GTAG:'G-BS0FWQNNNZ',
-                MAIN_SITENAME:'maxidoge',
-                MAIN_TWITTER:'@maxidoge',
-                OG_PATH:'/img/maxidoge/OG.jpeg'
-            ],
-            [ 
-                MAIN_DOMAIN:'https://maxidogtoken.com/',
-                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-                LOGO_PATH:'/img/maxidoge/logo.svg',
-                MAIN_GTAG:'G-BS0FWQNNNZ',
-                MAIN_SITENAME:'maxidoge',
-                MAIN_TWITTER:'@maxidoge',
-                OG_PATH:'/img/maxidoge/OG.jpeg'
-            ],
+          
         ]
 
     ]
