9110ee72af73dfcc0c15037164d447ab67a8fd9d
Steve <stevehuytrannd92@gmail.com>
2025-09-18 15:15:58 +0700

    remove domains

--- gitRepos/token6900.groovy
+++ gitRepos/token6900.groovy
@@ -9,24 +9,24 @@ return [
         vpsRef : 'vps1',   // 👈 just reference which VPS to use
 
         envs: [
-            [ 
-                MAIN_DOMAIN:'https://token6969.com/',
-                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-                LOGO_PATH:'/img/token6900/favicon.png',
-                MAIN_GTAG:'G-15Q5N6TLHT',
-                MAIN_SITENAME:'token6900',
-                MAIN_TWITTER:'@token6900',
-                OG_PATH:'/img/token6900/OG.jpeg'
-            ],
-            [ 
-                MAIN_DOMAIN:'https://spxtoken6900.com/',
-                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-                LOGO_PATH:'/img/token6900/favicon.png',
-                MAIN_GTAG:'G-15Q5N6TLHT',
-                MAIN_SITENAME:'token6900',
-                MAIN_TWITTER:'@token6900',
-                OG_PATH:'/img/token6900/OG.jpeg'
-            ],
+            // [ 
+            //     MAIN_DOMAIN:'https://token6969.com/',
+            //     BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+            //     LOGO_PATH:'/img/token6900/favicon.png',
+            //     MAIN_GTAG:'G-15Q5N6TLHT',
+            //     MAIN_SITENAME:'token6900',
+            //     MAIN_TWITTER:'@token6900',
+            //     OG_PATH:'/img/token6900/OG.jpeg'
+            // ],
+            // [ 
+            //     MAIN_DOMAIN:'https://spxtoken6900.com/',
+            //     BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+            //     LOGO_PATH:'/img/token6900/favicon.png',
+            //     MAIN_GTAG:'G-15Q5N6TLHT',
+            //     MAIN_SITENAME:'token6900',
+            //     MAIN_TWITTER:'@token6900',
+            //     OG_PATH:'/img/token6900/OG.jpeg'
+            // ],
         ]
 
     ],
--- gitRepos/wepe.groovy
+++ gitRepos/wepe.groovy
@@ -8,15 +8,15 @@ return [
 
         vpsRef : 'vps1',   // 👈 just reference which VPS to use
         envs: [
-            [ 
-                MAIN_DOMAIN:'https://wepetoken.com/',
-                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
-                LOGO_PATH:'/img/wepe/token.svg',
-                MAIN_GTAG:'G-LJLVSX3D6T',
-                MAIN_SITENAME:'wepetoken',
-                MAIN_TWITTER:'@wepetoken',
-                OG_PATH:'/img/wepe/OG.jpeg'
-            ],
+            // [ 
+            //     MAIN_DOMAIN:'https://wepetoken.com/',
+            //     BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+            //     LOGO_PATH:'/img/wepe/token.svg',
+            //     MAIN_GTAG:'G-LJLVSX3D6T',
+            //     MAIN_SITENAME:'wepetoken',
+            //     MAIN_TWITTER:'@wepetoken',
+            //     OG_PATH:'/img/wepe/OG.jpeg'
+            // ],
         ]
 
     ],
