6dc72eb47718027dcfc503656fed182a01350748
Steve <stevehuytrannd92@gmail.com>
2025-11-16 12:25:44 +0700

    add new domain

---
+++ gitRepos/liquidchain.groovy
@@ -0,0 +1,26 @@
+// repos/maxidoge.groovy
+return [
+    [
+        url: 'git@github.com-stevehuytrannd92/flockerz.git',
+        branch: 'landing_liquidchain',
+        folder: 'landing_liquidchain',
+        credId: 'id_ed25519_stevehuytrannd92',
+        buildType: 'nextjs',
+
+        vpsRef : 'vps1',   // 👈 just reference which VPS to use
+
+        envs: [
+          
+            [ 
+                MAIN_DOMAIN:'https://liquidchaintoken.com/',
+                BACKLINKS_URL:'https://btcsymbol.net/public/js/backlinks.json',
+                LOGO_PATH:'/img/liquidchain/token.svg',
+                MAIN_GTAG:'G-KGQ352LLBS',
+                MAIN_SITENAME:'liquidchain',
+                MAIN_TWITTER:'@liquidchain',
+                OG_PATH:'/img/liquidchain/OG.png'
+            ],
+        ]
+
+    ]
+]
